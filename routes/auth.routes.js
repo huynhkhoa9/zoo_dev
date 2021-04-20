3a6d49754583fdad333e4fb550a5e0da2a987470
huynhkhoa9 <huynhkhoa9@gmail.com>
2021-04-20 06:13:46 -0500

    server

---
+++ routes/auth.routes.js
@@ -0,0 +1,13 @@
+const { setRandomFallback } = require("bcryptjs");
+const controller = require("../controllers/auth.controller");
+const router = require("express").Router()
+
+router.post("/api/auth/signin", controller.signin)
+router.post("/api/auth/signup", controller.signup)
+router.post("/api/auth/employeesignin", controller.employeesignin)
+router.post("/api/auth/addanimal", controller.addanimal)
+router.post("/api/auth/addemployee", controller.addemployee)
+router.post("/api/auth/addrevenue", controller.addrevenue)
+router.post("/api/auth/getAnimals", controller.getAnimals)
+
+module.exports = router;
