fd96e32e4001e40d39b4563e0fccb3d49cb7f6fd
huynhkhoa9 <huynhkhoa9@gmail.com>
2021-04-20 04:58:08 -0500

    Update server.compiled.js

--- server.compiled.js
+++ server.compiled.js
@@ -13,20 +13,10 @@ var authroutes = require("./routes/auth.routes");
 var mysql = require('mysql');
 
 var PORT = process.env.PORT || 8080;
-var app = (0, _express["default"])(); // Serve the static files from the React app
+var app = (0, _express["default"])(); // add middlewares
 
-app.use(_express["default"]["static"](_path["default"].join(__dirname, 'client/build'))); // An api endpoint that returns a short list of items
-
-app.get('/api/getList', function (req, res) {
-  var list = ["item1", "item2", "item3"];
-  res.json(list);
-  console.log('Sent list of items');
-}); // Handles any requests that don't match the ones above
-
-app.get('*', function (req, res) {
-  res.sendFile(_path["default"].join(__dirname + '/client/build/index.html'));
-});
-app.use("/", authroutes);
+app.use(_express["default"]["static"](_path["default"].join(__dirname, "..", "build")));
+app.use(_express["default"]["static"]("public"));
 app.listen(PORT, function () {
   console.log("Server listening at port ".concat(PORT, "."));
 });
