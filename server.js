e8ab22026344637dc4c8ac9ca135a567061a7cbb
huynhkhoa9 <huynhkhoa9@gmail.com>
2021-04-20 03:40:28 -0500

    123

--- server.js
+++ server.js
@@ -3,12 +3,10 @@
 import path from 'path';
 import express from 'express';
 
-const authroutes = require("./routes/auth.routes")
 const PORT = process.env.PORT || 8080;
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'client', 'build')));
-app.use("/", authroutes)
 
 app.listen(PORT, () => {
   console.log(`Server listening at port ${PORT}.`);
