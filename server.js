a738c72b4c16131771b00e96a0c39b4ecd2898b5
huynhkhoa9 <huynhkhoa9@gmail.com>
2021-04-20 00:09:02 -0500

    init

--- server.js
+++ server.js
@@ -1,20 +1,23 @@
 // server.js
 //
-const express = require('express');
-const PORT = process.env.PORT || 4001;
+import path from 'path-dirname';
+import express from 'express';
 
+const PORT = process.env.HTTP_PORT || 4001;
 const app = express();
 
+app.use(express.static(path.join(__dirname, 'client', 'build')));
+
 app.get('/', (req, res) => {
-    res.send('just gonna send it');
+  res.send('just gonna send it');
+});
+
+app.get('/flower', (req, res) => {
+  res.json({
+    name: 'Dandelion',
+    colour: 'Blue-ish'
   });
-  
-  app.get('/flower', (req, res) => {
-    res.json({
-      name: 'Dandelion',
-      colour: 'Blue-ish'
-    });
-  });
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening at port ${PORT}.`);
