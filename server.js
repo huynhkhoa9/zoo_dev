8d5b6546162a8ac5a5a2c02193732ce8794c49eb
huynhkhoa9 <huynhkhoa9@gmail.com>
2021-04-20 04:57:48 -0500

    Update server.js

--- server.js
+++ server.js
@@ -8,22 +8,9 @@ const mysql = require('mysql')
 const PORT = process.env.PORT || 8080;
 const app = express();
 
-// Serve the static files from the React app
-app.use(express.static(path.join(__dirname, 'client/build')));
-
-// An api endpoint that returns a short list of items
-app.get('/api/getList', (req,res) => {
-    var list = ["item1", "item2", "item3"];
-    res.json(list);
-    console.log('Sent list of items');
-});
-
-// Handles any requests that don't match the ones above
-app.get('*', (req,res) =>{
-    res.sendFile(path.join(__dirname+'/client/build/index.html'));
-});
-
-app.use("/", authroutes);
+// add middlewares
+app.use(express.static(path.join(__dirname, "..", "build")));
+app.use(express.static("public"));
 
 app.listen(PORT, () => {
   console.log(`Server listening at port ${PORT}.`);
