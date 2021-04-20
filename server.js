53466a797b862c32c512943c1bdc27bca8a8538d
huynhkhoa9 <huynhkhoa9@gmail.com>
2021-04-20 02:30:50 -0500

    Update server.js

--- server.js
+++ server.js
@@ -8,16 +8,6 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'client', 'build')));
 
-app.get('/', (req, res) => {
-  res.send('just gonna send it');
-});
-
-app.get('/flower', (req, res) => {
-  res.json({
-    name: 'Dandelion',
-    colour: 'Blue-ish'
-  });
-});
 
 app.listen(PORT, () => {
   console.log(`Server listening at port ${PORT}.`);
