0567d7515e7cfc07476ab06e0603f3baabc8f0b0
huynhkhoa9 <huynhkhoa9@gmail.com>
2021-04-20 03:05:22 -0500

    1123

--- server.compiled.js
+++ server.compiled.js
@@ -8,10 +8,6 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "d
 
 // server.js
 //
-var bodyParser = require("body-parser");
-
-var cors = require("cors");
-
 var authroutes = require("./routes/auth.routes");
 
 var mysql = require('mysql');
@@ -19,13 +15,6 @@ var mysql = require('mysql');
 var PORT = process.env.PORT || 8080;
 var app = (0, _express["default"])();
 app.use(_express["default"]["static"](_path["default"].join(__dirname, 'client', 'build')));
-app.use(cors()); // parse requests of content-type - application/json
-
-app.use(bodyParser.json()); // parse requests of content-type - application/x-www-form-urlencoded
-
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
 app.use("/", authroutes);
 app.listen(PORT, function () {
   console.log("Server listening at port ".concat(PORT, "."));
