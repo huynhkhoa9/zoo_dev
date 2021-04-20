6239b87fd6c83a68e24a52bc06ffa39f9b93dd5c
huynhkhoa9 <huynhkhoa9@gmail.com>
2021-04-20 06:39:12 -0500

    server

--- server.compiled.js
+++ server.compiled.js
@@ -4,21 +4,33 @@ var _path = _interopRequireDefault(require("path"));
 
 var _express = _interopRequireDefault(require("express"));
 
-var _auth = require("./auth.routes");
-
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 // server.js
 //
 var PORT = process.env.PORT || 8080;
 var app = (0, _express["default"])();
-// Serve the static files from the React app
+var db = mysql.createPool({
+  host: process.env.RDS_HOSTNAME,
+  user: process.env.RDS_USERNAME,
+  password: process.env.RDS_PASSWORD,
+  database: process.env.RDS_HOSTNAME
+}); // Serve the static files from the React app
+
 app.use(_express["default"]["static"](_path["default"].join(__dirname, 'client/build'))); // Handles any requests that don't match the ones above
 
 app.get('*', function (req, res) {
   res.sendFile(_path["default"].join(__dirname + '/client/build/index.html'));
 });
-app.use("/", _auth.router);
+db.getConnection(function (err, connection) {
+  if (err) {
+    return console.error('error: ' + err.message);
+  } // execute query
+  // ...
+
+
+  connnection.release();
+});
 app.listen(PORT, function () {
   console.log("Server listening at port ".concat(PORT, "."));
 });
--- server.js
+++ server.js
@@ -5,7 +5,13 @@ import express from 'express';
 
 const PORT = process.env.PORT || 8080;
 const app = express();
-import {router} from './auth.routes';
+
+const db = mysql.createPool({
+  host: process.env.RDS_HOSTNAME,
+  user:  process.env.RDS_USERNAME,
+  password:  process.env.RDS_PASSWORD,
+  database:  process.env.RDS_HOSTNAME
+});
 
 // Serve the static files from the React app
 app.use(express.static(path.join(__dirname, 'client/build')));
@@ -14,7 +20,15 @@ app.get('*', (req,res) =>{
     res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
 
-app.use("/", router);
+db.getConnection(function(err, connection) {
+  if (err) {
+    return console.error('error: ' + err.message);
+  }
+  // execute query
+  // ...
+  connnection.release();
+});
+
 
 app.listen(PORT, () => {
   console.log(`Server listening at port ${PORT}.`);
