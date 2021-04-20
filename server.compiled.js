f735ef5e19898988c910114393c7f16d2085ab32
huynhkhoa9 <huynhkhoa9@gmail.com>
2021-04-20 04:10:13 -0500

    Update server.compiled.js

--- server.compiled.js
+++ server.compiled.js
@@ -11,6 +11,9 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "d
 var PORT = process.env.PORT || 8080;
 var app = (0, _express["default"])();
 app.use(_express["default"]["static"](_path["default"].join(__dirname, 'client', 'build')));
+app.get('/', function (req, res) {
+  res.sendFile(_path["default"].join(__dirname, 'build', 'index.html'));
+});
 app.listen(PORT, function () {
   console.log("Server listening at port ".concat(PORT, "."));
 });
