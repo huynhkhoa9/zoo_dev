aed2933b275b93152cba10aa6d7b5bc2d58e5242
huynhkhoa9 <huynhkhoa9@gmail.com>
2021-04-20 04:07:04 -0500

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'client', 'build')));
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'))
+})
 app.listen(PORT, () => {
   console.log(`Server listening at port ${PORT}.`);
 });
