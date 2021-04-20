fa21983dc16169a2b5a9f02e4fb8362807c24e24
huynhkhoa9 <huynhkhoa9@gmail.com>
2021-04-20 01:15:59 -0500

    123

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 import path from 'path';
 import express from 'express';
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 4001;
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'client', 'build')));
