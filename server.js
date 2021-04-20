d6943fd1abe4278edfeaf0742c197d6919c76375
huynhkhoa9 <huynhkhoa9@gmail.com>
2021-04-20 04:25:35 -0500

    server

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ import express from 'express';
 const PORT = process.env.PORT || 8080;
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'client', 'build')));
+//app.use(express.static(path.join(__dirname, 'client', 'build')));
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, 'client', 'public')));
 
 app.listen(PORT, () => {
   console.log(`Server listening at port ${PORT}.`);
