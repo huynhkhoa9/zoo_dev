1a3317c387936bd4861a5a9d45b6441fbfff6004
huynhkhoa9 <huynhkhoa9@gmail.com>
2021-04-19 23:36:09 -0500

    init

--- server.js
+++ server.js
@@ -6,8 +6,15 @@ const PORT = process.env.PORT || 4001;
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('flowers smell nice');
-});
+    res.send('just gonna send it');
+  });
+  
+  app.get('/flower', (req, res) => {
+    res.json({
+      name: 'Dandelion',
+      colour: 'Blue-ish'
+    });
+  });
 
 app.listen(PORT, () => {
   console.log(`Server listening at port ${PORT}.`);
