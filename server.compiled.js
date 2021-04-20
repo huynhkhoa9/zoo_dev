9210a73fb25e9c890bb9dd146b6aa16207db1b5b
huynhkhoa9 <huynhkhoa9@gmail.com>
2021-04-20 04:32:20 -0500

    server

--- server.compiled.js
+++ server.compiled.js
@@ -9,9 +9,19 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "d
 // server.js
 //
 var PORT = process.env.PORT || 8080;
-var app = (0, _express["default"])(); //app.use(express.static(path.join(__dirname, 'client', 'build')));
+var app = (0, _express["default"])(); // Serve the static files from the React app
 
-app.use(_express["default"]["static"](_path["default"].join(__dirname, 'client', 'public')));
+app.use(_express["default"]["static"](_path["default"].join(__dirname, 'client/build'))); // An api endpoint that returns a short list of items
+
+app.get('/api/getList', function (req, res) {
+  var list = ["item1", "item2", "item3"];
+  res.json(list);
+  console.log('Sent list of items');
+}); // Handles any requests that don't match the ones above
+
+app.get('*', function (req, res) {
+  res.sendFile(_path["default"].join(__dirname + '/client/build/index.html'));
+});
 app.listen(PORT, function () {
   console.log("Server listening at port ".concat(PORT, "."));
 });
--- server.js
+++ server.js
@@ -6,9 +6,20 @@ import express from 'express';
 const PORT = process.env.PORT || 8080;
 const app = express();
 
-//app.use(express.static(path.join(__dirname, 'client', 'build')));
+// Serve the static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.use(express.static(path.join(__dirname, 'client', 'public')));
+// An api endpoint that returns a short list of items
+app.get('/api/getList', (req,res) => {
+    var list = ["item1", "item2", "item3"];
+    res.json(list);
+    console.log('Sent list of items');
+});
+
+// Handles any requests that don't match the ones above
+app.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening at port ${PORT}.`);
